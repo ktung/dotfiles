d719a3e90ab78ff5a889c9aafbe15c834a178c38
ktung <ptung@protonmail.ch>
2022-08-10 17:22:52 -0400

    add vscode

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
 ln -s $(pwd)/git/.gitignore ~/.gitignore
+
+rm ~/Library/Application\ Support/Code/User/settings.json
+ln -s $(pwd)/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
